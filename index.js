55185363af3ba90d411bab56afb85d9f463e5f74
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-19 16:51:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 function takeANumber(deliLine, name) {
   
-  return `Welcome, ${name}.\n  You are number ${deliLine.length+1} in line.`
+  return `Welcome, ${name}. You are number ${deliLine.length+1} in line.`
 }
 
 
