5c55e5c2f34088bf2f0518c8fef1ff3bc583de31
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-19 16:35:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function takeANumber(deliLine, name) {
   
   return `Welcome, ${name}.\n  You are number ${deliLine.length+1} in line.`
 }
+
+
+function nowServing(deliLine) {
+  
+  var nextUp
+  
+  if (deliLine.length > 0) {
+    nextUp = deliLine.shift()
+  } else {
+    nextUp = "There is nobody waiting to be served!"
+  }
+  
+  return nextUp
+  
+}
+
